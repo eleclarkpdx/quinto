9fb82efb3d42f1c49b1d75e0ede1d5769e703f4e
Eleanore Clark <78524415+eleclarkpdx@users.noreply.github.com>
2022-06-06 00:53:31 -0700

    unit tests set up for game; get_tiles working

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+use rand::{Rng, distributions::*};
 
 pub fn init_board() -> [[Option<u8>; 13 ]; 13] {
     let board: [[Option<u8>; 13]; 13] = [[None; 13]; 13];
@@ -6,11 +6,14 @@ pub fn init_board() -> [[Option<u8>; 13 ]; 13] {
 }
 
 pub fn get_tiles(num_tiles_needed: u8) -> Box<Vec<u8>> {
-    let mut tiles: Vec<u8> = vec![];
+    if num_tiles_needed == 0 {
+        return Box::new(vec![]);
+    }
+    let between = Uniform::from(0..10);
     let mut rng = rand::thread_rng();
+    let mut tiles: Vec<u8> = vec![];
     for i in 0..(usize::from(num_tiles_needed)) {
-        tiles[i] = rng.gen();
-        println!("{:?}", tiles[i]);
+        tiles.push(between.sample(&mut rng));
     }
     Box::new(tiles)
 }
--- src/tests.rs
+++ src/tests.rs
@@ -2,9 +2,9 @@ use super::game;
 
 #[test]
 fn get_some_tiles() {
-    let mut tiles;
+    let mut tiles: Vec<u8> = vec![];
     for i in 0..10 {
-        tiles = game::get_tiles(i);
+        tiles = *game::get_tiles(i);
         println!("{:?}", tiles);
     }
 }
