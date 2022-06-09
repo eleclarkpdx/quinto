e6e41ddf5001bccea6bc7c5fd8138fef94b87d1f
PDXChristian <8883699+PDXChristian@users.noreply.github.com>
2022-06-08 18:14:44 -0700

    Switched to EJS with node

---
+++ front-end/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
